c7c1ec8f04fc743d474ca648000aac70bb5cac20
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-11-30 12:52:25 -0600

    Agregado de entidades
    Agregue todas las entidades que vamos a manejar con sus datos y relaciones

---
+++ app/src/main/java/com/redb/to_dolist/DB/Entidades/InvitacionEntity.kt
@@ -0,0 +1,20 @@
+package com.redb.to_dolist.DB.Entidades
+
+import androidx.room.*
+
+
+@Entity(tableName = "Invitacion",
+    foreignKeys = arrayOf(
+        ForeignKey(entity = UsuarioEntity::class,
+            parentColumns = arrayOf("idUsuario"),
+            childColumns = arrayOf("idUsuario"),
+            onDelete = ForeignKey.CASCADE)
+    ),
+    indices = [Index("idUsuario")]
+    )
+data class InvitacionEntity(
+    @PrimaryKey @ColumnInfo(name = "idInvitacion") val idInvitacion:Int,
+    @ColumnInfo(name = "idUsuario") val idUsuario:String,
+    @ColumnInfo(name = "idLista") val idLista:String,
+    @ColumnInfo(name = "acepted",typeAffinity = ColumnInfo.INTEGER) val acepted:Boolean
+)
