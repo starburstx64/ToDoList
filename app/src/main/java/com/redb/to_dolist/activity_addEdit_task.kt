ec728f18526d00630494e5792aa93be9c2158f67
KyoZeta <47518944+KyoZeta@users.noreply.github.com>
2019-12-02 17:37:58 -0600

    creacion de la vista add/edit task y adiciones al string.xml

---
+++ app/src/main/java/com/redb/to_dolist/activity_addEdit_task.kt
@@ -0,0 +1,12 @@
+package com.redb.to_dolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_addEdit_task : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_task)
+    }
+}
