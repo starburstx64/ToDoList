eeb2bfecb25a7db33709fbd7262c6534f8d41cfe
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-12-04 14:20:55 -0600

    correccion de un shingo de cosas y otras cosas que ya sirven

--- app/src/main/java/com/redb/to_dolist/Vistas/MenuPrincipalActivity.kt
+++ app/src/main/java/com/redb/to_dolist/Vistas/MenuPrincipalActivity.kt
@@ -16,11 +16,9 @@ import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import android.widget.TextView
 import com.facebook.stetho.Stetho
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.redb.to_dolist.DB.AppDatabase
+import com.redb.to_dolist.Modelos.FBModels.ListFB
 import com.redb.to_dolist.Modelos.FBModels.User
 import com.redb.to_dolist.R
 
@@ -31,6 +29,8 @@ class MenuPrincipalActivity : AppCompatActivity() {
     private lateinit var tvUserName:TextView
     private lateinit var tvUserMail:TextView
     private lateinit var btnSincronizar:ClipData.Item
+
+    private val database=FirebaseDatabase.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,11 +75,13 @@ class MenuPrincipalActivity : AppCompatActivity() {
         }
 
         db.getAplicacionDao().getAll()
-        val database=FirebaseDatabase.getInstance()
+
+
 
 //      val userReference1=database.getReference("App").child("users").setValue("holita")
-//        val userReference=database.getReference("App").child("users").orderByChild("email").equalTo("david@hotmail.com")
+        //val userReference=database.getReference("App").child("users").orderByChild("email").equalTo("raul@hotmail.com")
         val userReference=database.getReference("App").child("users").orderByKey().equalTo("raulhotmailcom")
+        //database.getReference("App").child("prueba").setValue("holita")
         userReference.addChildEventListener(object : ChildEventListener{
             override fun onChildChanged(p0: DataSnapshot, p1: String?) {
                 val user:User? = p0.getValue(User::class.java)
@@ -110,7 +112,7 @@ class MenuPrincipalActivity : AppCompatActivity() {
 
 
 
-        addMenuItemInNavMenuDrawer()
+        addListItems()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -124,8 +126,7 @@ class MenuPrincipalActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-
-    private fun addMenuItemInNavMenuDrawer() {
+    private fun addListItems() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView : NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -137,17 +138,61 @@ class MenuPrincipalActivity : AppCompatActivity() {
                 R.id.nav_add_lists
             ), drawerLayout
         )
-
+        //Obteniendo el menu
         val menu = navView.menu
 
-        val item = menu.add(0,Menu.NONE,1,"Listas").setIcon(R.drawable.ic_feedback_black_24dp).setOnMenuItemClickListener {
-            tvUserName.setText("hola")
-            navController.navigate(R.id.nav_home)
-            true
-        }
-        //item.setActionView(R.layout.fragment_home)
+        //obtener la llave de nuestro usuario
 
-        menu.add(0,Menu.NONE,1,"Listas2")
+        val idUsuario="raulhotmailcom"
+        //OBTENER AL OBJETO USUARIO PARA OBTENER LAS LISTAS QUE TIENE
+        val userReference=database.getReference("App").child("users").child(idUsuario)
+        userReference.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val currentUser:User? = p0.getValue(User::class.java)
+
+                var idsListas = currentUser!!.lists.keys
+
+                menu.removeGroup(0)
+                menu.removeGroup(1)
+                //menu.removeGroup(R.id.group_listas)
+
+                for (idLista in idsListas)
+                {
+                    val listReference=database.getReference("App").child("lists").child(idLista)
+                    listReference.addListenerForSingleValueEvent(object : ValueEventListener{
+                        override fun onCancelled(p0: DatabaseError) {
+                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                        }
+                        override fun onDataChange(p0: DataSnapshot) {
+                            val currentList:ListFB? = p0.getValue(ListFB::class.java)
+
+                            if(currentList!!.shared==false)
+                            {
+                                val item = menu.add(0,Menu.NONE,1,currentList.title)
+                            }
+                            else
+                            {
+                                val item =menu.add(1,Menu.NONE,2,currentList.title)
+                            }
+                        }
+                    })
+                }
+            }
+        })
+
+
+//        val item = menu.add(0,Menu.NONE,1,"Listas").setIcon(R.drawable.ic_feedback_black_24dp).setOnMenuItemClickListener {
+//            tvUserName.setText("hola")
+//            navController.navigate(R.id.nav_home)
+//            true
+//        }
+//        //item.setActionView(R.layout.fragment_home)
+//
+//        menu.add(0,Menu.NONE,1,"Listas2")
 
         navView.invalidate()
     }
