30e1608744f68facf76c7bf0cc8308c92c5b233a
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-12-04 19:35:42 -0600

    Agregado de filtros de tareas

--- app/src/main/java/com/redb/to_dolist/Modelos/FBModels/Task.kt
+++ app/src/main/java/com/redb/to_dolist/Modelos/FBModels/Task.kt
@@ -3,7 +3,7 @@ package com.redb.to_dolist.Modelos.FBModels
 data class Task (
     var title:String="",
     var importance:Int=0,
-    var duedate:Int=20000101,
+    var duedate:Int?=null,
     var completed:Boolean=false,
     var creator:String="",
     var creatorName:String="",
